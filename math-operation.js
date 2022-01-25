12787e5be1800d907cf0d752f8580f9d16cb2b17
shahriarshadhin <shadhin19shahriar@gmail.com>
2022-01-26 02:27:40 +0600

    added math operation

---
+++ math-operation.js
@@ -0,0 +1,21 @@
+var num1 = 25;
+var num2 = 20;
+
+var sum = num1 + num2;
+console.log(sum)
+
+var subtract = num1 - num2;
+console.log(subtract)
+
+var mult = num1 * num2;
+console.log(mult)
+
+var div = num1 / num2;
+console.log(div)
+
+// shorthand
+num2 += 3;
+console.log('num2 =', num2)
+
+num1++;
+console.log('num1 =', num1)
