e1be1dcd3150963f233304137c0bd506d6fe1620
shahriarshadhin <shadhin19shahriar@gmail.com>
2022-01-27 01:04:45 +0600

    added compare

--- math-operation.js
+++ math-operation.js
@@ -13,9 +13,16 @@ console.log(mult)
 var div = num1 / num2;
 console.log(div)
 
+
 // shorthand
 num2 += 3;
 console.log('num2 =', num2)
 
 num1++;
 console.log('num1 =', num1)
+console.log(17 % 5)
+
+var num3 = '99.5';
+var num4 = 15;
+
+console.log(num3 + num4)
