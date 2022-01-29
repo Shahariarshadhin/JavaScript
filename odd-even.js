3e0bf4a92660f742b5377c10c7696e561c4be72f
shahriarshadhin <shadhin19shahriar@gmail.com>
2022-01-30 01:59:33 +0600

    added even odd using function

---
+++ odd-even.js
@@ -0,0 +1,54 @@
+function even(number) {
+    if (number % 2 == 0) {
+        return true;
+    }
+    return false;
+}
+
+//practice
+
+const TheNumber = 7220;
+const thenum = even(TheNumber);
+console.log(thenum);
+
+function even(num) {
+    if (num % 2 == 0) {
+        return true;
+
+    }
+    return false;
+
+}
+
+const Number = 1221;
+const number = even(Number);
+console.log(number);
+
+
+// Odd
+
+function odd(number) {
+    if (number % 2 != 0) {
+        return true;
+    }
+}
+
+const isOdd = 1001;
+const Oddnumber = odd(isOdd);
+console.log(Oddnumber);
+
+//practice
+
+function odd(num) {
+    //if (num % 2 == 1) same as like down condition
+    if (num % 2 != 0) {
+        return true;
+
+    }
+    return false;
+
+}
+
+const isodd = 1012;
+const Oddnum = odd(isodd);
+console.log(Oddnum);
