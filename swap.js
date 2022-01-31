9a0b782e0c1e96186e9b9c0fe1ebb198cccd9311
shahriarshadhin <shadhin19shahriar@gmail.com>
2022-02-01 01:46:37 +0600

    added swap

---
+++ swap.js
@@ -0,0 +1,21 @@
+var firstNumber = 4;
+var secondNumber = 7;
+
+console.log(firstNumber, secondNumber);
+
+
+/* var temp = firstNumber;
+firstNumber = secondNumber;
+secondNumber = temp;
+
+console.log(firstNumber, secondNumber) */
+
+[firstNumber, secondNumber] = [secondNumber, firstNumber]
+console.log(firstNumber, secondNumber)
+
+/* let a = 5;
+let b = 7;
+console.log(a, b);
+
+[a, b] = [b, a];
+console.log(a, b) */
